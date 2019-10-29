e4bafd8a9c4de178da20c6792df407e334874267
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2019-10-29 11:30:36 +0000

    the connection holder is killing me.

--- spinncer/cerebellum.py
+++ spinncer/cerebellum.py
@@ -240,7 +240,7 @@ class Cerebellum(Circuit):
                 continue
             print("Retrieving connectivity for projection ", label, "...")
             all_connections[label] = \
-                np.array(p.get(('weight', 'delay'), format="list"))
+                np.array(p.get(('weight', 'delay'), format="list").connections)
         return all_connections
 
     def retrieve_population_names(self):
