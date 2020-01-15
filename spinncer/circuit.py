6a08a0006c33fe7afc288f7e8a9dbc4e5278d730
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2020-01-15 11:28:55 +0100

    removing Circuit dependency on spynnaker (spinn_utilities)

--- spinncer/circuit.py
+++ spinncer/circuit.py
@@ -1,17 +1,9 @@
-from six import add_metaclass
-from spinn_utilities.abstract_base import (
-    AbstractBase, abstractproperty, abstractmethod)
-
-
-@add_metaclass(AbstractBase)
 class Circuit(object):
 
-    @abstractmethod
     def get_circuit_inputs(self):
         """ Get a list of input populations belonging to the Circuit
         """
 
-    @abstractmethod
     def get_circuit_outputs(self):
         """ Get a list of output populations belonging to the Circuit
         """
