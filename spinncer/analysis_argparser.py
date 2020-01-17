17093cdd5659d09b3a56e31a1c7ec2374c1287b8
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2020-01-17 11:06:41 +0000

    fixed description of input

--- spinncer/analysis_argparser.py
+++ spinncer/analysis_argparser.py
@@ -6,10 +6,9 @@ analysis_parser = argparse.ArgumentParser(
     description='Analyse a cerebellar simulation run on SpiNNaker or  NEST.',
     formatter_class=argparse.RawTextHelpFormatter)
 
-
 analysis_parser.add_argument('-i', '--input', type=str, nargs="*",
-                             help="name of the npz archive storing "
-                         "the results from running the simulations",
+                             help="name(s) of the npz archive storing "
+                                  "the results from running the simulations",
                              dest='input')
 
 analysis_parser.add_argument('--figures_dir', type=str,
