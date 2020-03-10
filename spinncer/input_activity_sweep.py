1ddf7af7fcdd67c6494f14c8faf74cbf31b708ac
Petrut Bogdan <petrut.bogdan@manchester.ac.uk>
2020-03-10 13:53:12 +0000

    skeleton for input firing rate and stim radius sweep

---
+++ spinncer/input_activity_sweep.py
@@ -0,0 +1,122 @@
+"""
+Batch runner adapted from '
+'https://github.com/pabogdan/neurogenesis/blob/master/synaptogenesis/batch_argparser.py'
+'and '
+'https://github.com/pabogdan/neurogenesis/blob/master/synaptogenesis/batch_runner.py
+"""
+
+import subprocess
+import os
+import numpy as np
+import sys
+import hashlib
+import pylab as plt
+from spinncer.batch_argparser import *
+import shutil
+
+
+currrent_time = plt.datetime.datetime.now()
+string_time = currrent_time.strftime("%H%M%S_%d%m%Y")
+
+if args.suffix:
+    suffix = args.suffix
+else:
+    suffix = hashlib.md5(string_time.encode('utf-8')).hexdigest()
+
+# Some constants
+NO_CPUS = args.no_cpus
+MAX_CONCURRENT_PROCESSES = args.max_processes
+
+POISSON_PHASE = 0
+PERIODIC_PHASE = 1
+PHASES = [POISSON_PHASE, PERIODIC_PHASE]
+PHASES_NAMES = ["poisson", "periodic"]
+PHASES_ARGS = [None, "--periodic_stimulus"]
+
+concurrently_active_processes = 0
+
+f_peaks = np.arange(10, 200, 20)  # Hz
+radii = np.arange(40, 200, 20)  # um
+# TODO Fix ring buffer left shift value from previous experiment
+RB_LEFT_SHIFT = None
+
+# Compute total number of runs
+total_runs = f_peaks.size * len(PHASES) * radii.size
+
+parameters_of_interest = {
+    'f_peak': f_peaks,
+    'stim_radius': radii
+}
+
+dataset = "scaffold_full_dcn_400.0x400.0_v3.hdf5"
+
+log_calls = []
+
+# making a directory for this experiment
+dir_name = "activity_sweep_@{}".format(suffix)
+print("=" * 80)
+print("TOTAL RUNS", total_runs)
+print("MKDIR", dir_name)
+os.mkdir(dir_name)
+print("CHDIR", dir_name)
+os.chdir(dir_name)
+print("GETCWD", os.getcwd())
+print("-" * 80)
+
+for phase in PHASES:
+    for f_peak in f_peaks:
+        for stim_radius in radii:
+            filename = "spinn_400x400" \
+                       "_f_peak_{}" \
+                       "_stim_radius_{}" \
+                       "_{}" \
+                       "_@{}".format(f_peak,
+                                     stim_radius,
+                                     PHASES_NAMES[phase],
+                                     suffix)
+            # making a directory for this individual experiment
+            os.mkdir(filename)
+            os.chdir(filename)
+            print("GETCWD", os.getcwd())
+            shutil.copyfile("../../spynnaker.cfg", "spynnaker.cfg")
+            os.mknod("results.etxt")
+            os.mknod("results.otxt")
+
+            concurrently_active_processes += 1
+            null = open(os.devnull, 'w')
+            print("Run ", concurrently_active_processes, "...")
+
+            call = [sys.executable,
+                    '../../cerebellum_experiment.py',
+                    '--input', dataset,
+                    '-o', filename,
+                    '--f_peak', f_peak,
+                    '--stim_radius', stim_radius
+                    ]
+            for rbls in RB_LEFT_SHIFT:
+                call.append(str(rbls))
+
+            if PHASES_ARGS[phase] is not None:
+                call.append(PHASES_ARGS[phase])
+            print("CALL", call)
+            log_calls.append(call)
+            if concurrently_active_processes % MAX_CONCURRENT_PROCESSES == 0 \
+                    or concurrently_active_processes == total_runs:
+                # Blocking
+                with open("results.out", "wb") as out, open("results.err", "wb") as err:
+                    subprocess.call(call, stdout=out, stderr=err)
+                print("{} sims done".format(concurrently_active_processes))
+            else:
+                # Non-blocking
+                with open("results.out", "wb") as out, open("results.err", "wb") as err:
+                    subprocess.Popen(call, stdout=out, stderr=err)
+            os.chdir("..")
+            print("=" * 80)
+print("All done!")
+
+end_time = plt.datetime.datetime.now()
+total_time = end_time - currrent_time
+np.savez_compressed("batch_{}".format(suffix),
+                    parameters_of_interest=parameters_of_interest,
+                    total_time=total_time,
+                    log_calls=log_calls)
